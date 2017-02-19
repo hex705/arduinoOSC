f9f29677223678a4fbd5f1e5a0a4adb1df8457af
Steve Daniels <stevedaniels@ribosome.gateway.2wire.net>
2017-02-19 09:18:07 -0500

    Split out NetAddressInterface

---
+++ OSCNetAddress.cpp
@@ -0,0 +1,22 @@
+
+
+//=============================================================================
+// NetAddress interface
+//=============================================================================
+
+
+NetAddress  :: NetAddress   (){;}  // sets up object
+
+void NetAddress::set(IPAddress _ip, int _port) {
+    destinationIP = _ip;
+    destinationPort = _port;
+}
+
+IPAddress NetAddress :: getIP (){
+    return destinationIP;
+}
+
+int NetAddress :: getPort(){
+    return destinationPort;
+}
+
---
+++ OSCNetAddress.h
@@ -0,0 +1,21 @@
+//=============================================================================
+
+class NetAddress {
+    
+
+ protected:
+	
+    IPAddress destinationIP;
+    int       destinationPort; 
+    
+public:
+
+    
+    NetAddress(void);
+    void set (IPAddress, int);
+    IPAddress getIP();
+    int getPort();
+
+    
+};
+
