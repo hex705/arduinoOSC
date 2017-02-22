01897bafcfd94602f2932fceb1955116bae08b45
Steve Daniels <stevedaniels@ribosome.local>
2017-02-22 17:09:52 -0500

    example clean up

--- examples/serialUSBbasics/serialUSBbasics.ino
+++ examples/serialUSBbasics/serialUSBbasics.ino
@@ -1,21 +1,23 @@
-
 #include <OscSerial.h>
 
-// tile support libraries
+// tile support
 //#include <EthernetUdp.h>
-//#include <SPI.h>  
+//#include <SPI.h>
 
 OscSerial oscSerial;
-long timer;
+int baudRate = 9600;
+
+long serialTimerEnd;
+long serialTimerInterval = 100;
 
 void setup() {
-  Serial.begin(9600);
-  oscSerial.begin(Serial); 
-  pinMode(13, OUTPUT);
+    Serial.begin(baudRate); // has to be serial1 not Serial
+    oscSerial.beginUSB(Serial); // has to be beginUSB not just begin
+    pinMode(13,OUTPUT);
 }
 
 void loop() {
-  //  limits sends to every 100 mS
+    //  limits sends to every 100 mS
     if (millis()>= serialTimerEnd) {
         sendOscSerial();
         serialTimerEnd = millis() + serialTimerInterval;
@@ -24,6 +26,7 @@ void loop() {
     // important! this is needed in order to receive messages  
     oscSerial.listen();
 }
+
 // incoming messages will arrive here
 void oscEvent(OscMessage & msg) {
     // handle data here 
@@ -42,7 +45,7 @@ void myFunction(OscMessage & msg) {
 }
 // send OSC Serial messages
 void sendOscSerial() {
-    OscMessage msg("/outgoing"); // this could be any pattern
+    OscMessage msg("/pigsRUnFast"); // this could be any pattern
     msg.add(analogRead(A0)); // this could be any data
     oscSerial.send(msg);
 }
